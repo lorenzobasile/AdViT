ca5cd213b8caf402fa60eebe5df36a001ec965b3
lorenzobasile <lbasile14@hotmail.it>
2021-06-18 17:40:12 +0200

    subsampling for vit

--- vit2.py
+++ vit2.py
@@ -6,12 +6,30 @@ import torchvision
 from torchvision import transforms, models
 from utils import WarmupCosineSchedule
 
-model = timm.create_model('vit_base_patch16_224', pretrained=True, num_classes=10, img_size=96)
+model = timm.create_model('vit_base_patch16_224', pretrained=True, num_classes=10, img_size=32)
 device="cuda:0"
-model = model.to(device)
+
+for p in model.named_parameters():
+    if p[0]=='patch_embed.proj.bias':
+        biases=p[1]
+    if p[0]=='patch_embed.proj.weight':
+        weights=p[1]
+
+sampled_weights=weights[:,:,::4,::4]
+
+model.patch_embed.proj=nn.Conv2d(3,768,(4,4),(4,4))
+
+model.patch_embed.proj.weight=torch.nn.Parameter(sampled_weights)
+model.patch_embed.proj.bias=torch.nn.Parameter(biases)
+
+model.patch_embed.num_patches=64
+
+model.pos_embed = nn.Parameter(torch.zeros(1, 64 + 1, 768))
+
+model=model.to(device)
 
 transform=transforms.Compose([
-    transforms.Resize((96, 96)),
+    #transforms.Resize((96, 96)),
     transforms.ToTensor(),
     transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5]),
 ])
@@ -19,12 +37,12 @@ transform=transforms.Compose([
 trainset = torchvision.datasets.CIFAR10("./data", train=True, transform=transform, download=True)
 trainloader = torch.utils.data.DataLoader(trainset, batch_size=128, shuffle=True)
 testset = torchvision.datasets.CIFAR10("./data", train=False, transform=transform, download=True)
-testloader = torch.utils.data.DataLoader(testset, batch_size=128, shuffle=False)
+testloader = torch.utils.data.DataLoader(testset, batch_size=512, shuffle=False)
 
 #optimizer = torch.optim.Adam(model.parameters(), lr=0.03)
 optimizer = torch.optim.SGD(model.parameters(), lr=3e-2, momentum=0.9, weight_decay=0)
 loss = torch.nn.CrossEntropyLoss()
-scheduler=WarmupCosineSchedule(optimizer, warmup_steps=500, t_total=500)
+scheduler=WarmupCosineSchedule(optimizer, warmup_steps=500, t_total=1000)
 
 for epoch in range(500):
     print("Epoch: ", epoch, '/', 500)
