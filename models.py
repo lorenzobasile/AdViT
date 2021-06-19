0b44ad40d6a9df57f95688f1c5359a99dedfbcda
Lorenzo Basile <56440763+lorenzobasile@users.noreply.github.com>
2021-06-19 12:06:33 +0200

    imagenette fine tuning

---
+++ models.py
@@ -0,0 +1,20 @@
+import timm
+import torch
+import torch.nn as nn
+
+def create_ViT(img_size=224, patch_size=16, num_classes=10):
+    model=timm.create_model('vit_base_patch16_224', pretrained=True, num_classes=num_classes, img_size=img_size)
+    if patch_size!=16:
+        for p in model.named_parameters():
+            if p[0]=='patch_embed.proj.bias':
+                biases=p[1]
+            if p[0]=='patch_embed.proj.weight':
+                weights=p[1]
+        sampling_step=16//patch_size
+        sampled_weights=weights[:,:,::sampling_step,::sampling_step]
+        model.patch_embed.proj=nn.Conv2d(3, 768, (patch_size, patch_size), (patch_size, patch_size))
+        model.patch_embed.proj.weight=nn.Parameter(sampled_weights)
+        model.patch_embed.proj.bias=nn.Parameter(biases)
+        model.patch_embed.num_patches=(img_size//patch_size)**2
+        model.pos_embed=nn.Parameter(torch.zeros(1, model.patch_embed.num_patches+1, 768))
+    return model
--- vit_cifar.py
+++ vit_cifar.py
@@ -3,29 +3,31 @@ import torch
 import torch.nn as nn
 import numpy as np
 import torchvision
-from torchvision import transforms
+from torchvision import transforms, models
 from utils import WarmupCosineSchedule
-from models import create_ViT
 
-model = create_ViT(img_size=32, patch_size=4, num_classes=10)
+model = timm.create_model('vit_base_patch16_224', pretrained=True, num_classes=10, img_size=96)
 device="cuda:0"
-
-model=model.to(device)
+model = model.to(device)
 
 transform=transforms.Compose([
+    #transforms.Resize((96, 96)),
     transforms.ToTensor(),
     transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5]),
 ])
 
-trainset = torchvision.datasets.CIFAR10("./data", train=True, transform=transform, download=True)
+#trainset = torchvision.datasets.CIFAR10("./data", train=True, transform=transform, download=True)
+trainset=torchvision.datasets.STL10("./data", 'train', transform=transform, download=True)
 trainloader = torch.utils.data.DataLoader(trainset, batch_size=128, shuffle=True)
-testset = torchvision.datasets.CIFAR10("./data", train=False, transform=transform, download=True)
-testloader = torch.utils.data.DataLoader(testset, batch_size=512, shuffle=False)
+testset=torchvision.datasets.STL10("./data", 'test', transform=transform, download=True)
+
+#testset = torchvision.datasets.CIFAR10("./data", train=False, transform=transform, download=True)
+testloader = torch.utils.data.DataLoader(testset, batch_size=128, shuffle=False)
 
 #optimizer = torch.optim.Adam(model.parameters(), lr=0.03)
 optimizer = torch.optim.SGD(model.parameters(), lr=3e-2, momentum=0.9, weight_decay=0)
 loss = torch.nn.CrossEntropyLoss()
-scheduler=WarmupCosineSchedule(optimizer, warmup_steps=500, t_total=1000)
+scheduler=WarmupCosineSchedule(optimizer, warmup_steps=500, t_total=500)
 
 for epoch in range(500):
     print("Epoch: ", epoch, '/', 500)
@@ -54,4 +56,4 @@ for epoch in range(500):
             out=model(x.to(device))
             correct+=(torch.argmax(out, axis=1)==y.to(device)).sum().item()
     print("Accuracy on test set: ", correct/len(testset))
-torch.save(model.state_dict(), "./trained_models/cifar_vit.pt")
+torch.save(model.state_dict(), "./models/vit_sgd.pt")
