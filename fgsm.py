122e4d591f8def774f072a6d8982b464465e5c49
Lorenzo Basile <56440763+lorenzobasile@users.noreply.github.com>
2021-06-22 22:14:13 +0200

    testing fgsm

--- fgsm.py
+++ fgsm.py
@@ -24,20 +24,34 @@ data_dir = './data/imagenette2-320/'
 datasets = {x: torchvision.datasets.ImageFolder(os.path.join(data_dir, x), data_transforms[x]) for x in ['train', 'test']}
 dataloaders = {'train': DataLoader(datasets['train'], batch_size=128, shuffle=True),'test': DataLoader(datasets['test'], batch_size=64, shuffle=False)}
 
+model_names=['resnet18', 'tv_resnet50', 'tv_resnet101', 'vgg16', 'vit_base_patch16_224',  'vit_small_patch16_224', 'vit_base_patch32_224',  'vit_small_patch32_224']
+
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
-model=timm.create_model('resnet18', pretrained=True, num_classes=10)
+models=[timm.create_model(model_name, pretrained=True, num_classes=10).to(device) for model_name in model_names]
 
-model=model.to(device)
-model.fc.load_state_dict(torch.load("./trained_models/resnet18.pt"))
-model.eval()
-adversary=FGSM(model, 'cuda')
-correct=0
-#with torch.no_grad():
-for x, y in dataloaders['test']:
-    x=x.to(device)
-    y=y.to(device)
-    x=adversary.generate(x, y, epsilon=0.1)
-    out=model(x)
-    correct+=(torch.argmax(out, axis=1)==y).sum().item()
-print("Accuracy on test set: ", correct/len(dataloaders['test'].dataset))
+#models={key: timm.create_model(key, pretrained=True, num_classes=10).to(device) for key in model_names}
+
+for i, model_name in enumerate(model_names):
+    if 'vit' in model_name:
+        models[i].head.load_state_dict(torch.load("./trained_models/in1k"+model_name[4:-4]+".pt"))
+    if 'vgg' in model_name:
+        models[i].head.fc.load_state_dict(torch.load("./trained_models/vgg16.pt"))
+    else:
+        models[i].fc.load_state_dict(torch.load("./trained_models/"+model_name+".pt"))
+    models[i].eval()
+
+#adversaries={key: FGSM(models[key], 'cuda') for key in model_names}
+adversaries=[FGSM(model, 'cuda') for model in models]
+
+for i, attacked_model in enumerate(models):
+    correct=torch.zeros(len(models))
+    out=torch.empty((len(models), 128, 10))
+    for x, y in dataloaders['test']:
+        x=x.to(device)
+        y=y.to(device)
+        x=adversaries[i].generate(x, y, epsilon=0.1)
+        for k, model in enumerate(models):
+            out[k]=model(x)
+            correct[k]+=torch.argmax(out[k], axis=1)==y).sum().item()
+    #print("Accuracy on test set: ", correct/len(dataloaders['test'].dataset))
