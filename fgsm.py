820c92c50803453931a0857f5463929bc9242991
Lorenzo Basile <56440763+lorenzobasile@users.noreply.github.com>
2021-06-23 10:41:57 +0200

    automated fgsm script

--- fgsm.py
+++ fgsm.py
@@ -6,6 +6,8 @@ from torchvision import transforms
 import torchvision
 import os
 
+outfile_name="./attack_results/fgsm.txt"
+
 data_transforms = {
     'train': transforms.Compose([
         transforms.Resize((224,224)),
@@ -25,14 +27,12 @@ datasets = {x: torchvision.datasets.ImageFolder(os.path.join(data_dir, x), data_
 dataloaders = {'train': DataLoader(datasets['train'], batch_size=128, shuffle=True),'test': DataLoader(datasets['test'], batch_size=64, shuffle=False)}
 
 model_names=['resnet18', 'tv_resnet50', 'tv_resnet101', 'vgg16', 'vit_base_patch16_224',  'vit_base_patch32_224',  'vit_small_patch16_224','vit_small_patch32_224']
-#model_names=['vit_small_patch16_224']
-
+epsilons=[0.001, 0.005, 0.01, 0.05, 0.1]
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
 models=[timm.create_model(model_name, pretrained=True, num_classes=10).to(device) for model_name in model_names]
 
-#models={key: timm.create_model(key, pretrained=True, num_classes=10).to(device) for key in model_names}
 
 for i, model_name in enumerate(model_names):
     print(model_name)
@@ -44,47 +44,29 @@ for i, model_name in enumerate(model_names):
         models[i].fc.load_state_dict(torch.load("./trained_models/"+model_name+".pt"))
     models[i].eval()
 correct=0
-#adversaries={key: FGSM(models[key], 'cuda') for key in model_names}
+
+correct=torch.zeros(len(models))
+for x, y in dataloaders['test']:
+        x=x.to(device)
+        y=y.to(device)
+        for k, model in enumerate(models):
+            temp=torch.argmax(model(x), axis=1)==y
+            correct[k]+=temp.sum().item()
+with open(outfile_name, 'w') as outfile:
+    outfile.write("Clean accuracy: ", correct/len(dataloaders['test'].dataset))
+print("Clean accuracy: ", correct/len(dataloaders['test'].dataset))
+
 adversaries=[FGSM(model, 'cuda') for model in models]
-correct=torch.zeros(len(models))
-for x, y in dataloaders['test']:
-        x=x.to(device)
-        y=y.to(device)
-        for k, model in enumerate(models):
-            temp=torch.argmax(model(x), axis=1)==y
-            correct[k]+=temp.sum().item()
-print(correct/len(dataloaders['test'].dataset))
-
-for i, attacked_model in enumerate(models):
-    print(i)
-    correct=torch.zeros(len(models))
-    for x, y in dataloaders['test']:
-        x=x.to(device)
-        y=y.to(device)
-        x=adversaries[i].generate(x, y, epsilon=0.1)
-        for k, model in enumerate(models):
-            temp=torch.argmax(model(x), axis=1)==y
-            correct[k]+=temp.sum().item()
-    print(correct/len(dataloaders['test'].dataset))
-    #print("Accuracy on test set: ", correct/len(dataloaders['test'].dataset))
-'''
-out1=torch.empty((len(models), 64, 10)).to(device)
-for x, y in dataloaders['test']:
-    x=x.to(device)
-    y=y.to(device)
-    x=adversaries[i].generate(x, y, epsilon=0.1)
-    out=models[0](x)
-    correct+=(torch.argmax(out, axis=1)==y).sum().item()
-
-correct=torch.zeros(len(models))
-out=torch.empty((len(models), 64, 10)).to(device)
-for pippo,(x, y) in enumerate(dataloaders['test']):
-    print(pippo)
-    x=x.to(device)
-    y=y.to(device)
-    x=adversaries[i].generate(x, y, epsilon=0.1)
-    for k, model in enumerate(models):
-        #out[k]=model(x)
-        correct[k]+=(torch.argmax(model(x), axis=1)==y).sum().item()
-        #correct[k]+=temp.sum().item()
-'''
+for eps in epsilons:
+    for i, attacked_model in enumerate(models):
+        correct=torch.zeros(len(models))
+        for x, y in dataloaders['test']:
+            x=x.to(device)
+            y=y.to(device)
+            x=adversaries[i].generate(x, y, epsilon=eps)
+            for k, model in enumerate(models):
+                temp=torch.argmax(model(x), axis=1)==y
+                correct[k]+=temp.sum().item()
+        with open(outfile_name, 'a') as outfile:
+             outfile.write("\nAttack on "+model_names[i]+": ", correct/len(dataloaders['test'].dataset))
+        print("Attack on "+model_names[i]+": ", correct/len(dataloaders['test'].dataset))
