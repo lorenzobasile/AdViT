1b8eb89306113751718cef6a6763dfffe9635090
Roberto <robi290596@gmail.com>
2021-06-29 00:10:27 +0200

    added PGD training

---
+++ utils/adversarial_train.py
@@ -0,0 +1,147 @@
+import torch
+from deeprobust.image.attack.fgsm import FGSM
+from deeprobust.image.attack.pgd import PGD
+
+
+def FGSMtrain(model, dataloaders, n_epochs, optimizer, eps, scheduler=None, outfile_name=None, clip=False):
+
+    loss=torch.nn.CrossEntropyLoss()
+    device=torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    if outfile_name is not None:
+        with open(outfile_name, 'w') as outfile:
+            outfile.write("")
+    adversary = FGSM(model, 'cuda')
+
+
+    for epoch in range(n_epochs):
+
+        if outfile_name is not None:
+            with open(outfile_name, 'a') as outfile:
+                outfile.write("\nEpoch: "+str(epoch)+'/'+str(n_epochs))
+        print("Epoch: ", epoch, '/', n_epochs)
+
+        model.train()
+        for x, y in dataloaders['train']:
+            x=x.to(device)
+            y=y.to(device)
+            x_adv = adversary.generate(x, y, epsilon=eps)
+            out=model(x_adv)
+            l=loss(out, y)
+            optimizer.zero_grad()
+            l.backward()
+            if clip:
+                torch.nn.utils.clip_grad_norm_(model.parameters(), 1.0)
+            optimizer.step()
+        if scheduler is not None:
+            scheduler.step()
+
+        model.eval()
+        correct = 0
+        with torch.no_grad():
+            for x, y in dataloaders['train']:
+                out = model(x.to(device))
+                correct += (torch.argmax(out, axis=1) == y.to(device)).sum().item()
+        if outfile_name is not None:
+            with open(outfile_name, 'a') as outfile:
+                outfile.write(f"\n\nClean Accuracy on training set: {correct / len(dataloaders['train'].dataset) * 100:.5f} %")
+        print(f"\n\nClean Accuracy on training set: {correct / len(dataloaders['train'].dataset) * 100:.5f} %")
+
+        correct_adv = 0
+        for x, y in dataloaders['train']:
+            out_adv = model(adversary.generate(x.to(device), y.to(device), epsilon=eps))
+            correct_adv += (torch.argmax(out_adv, axis=1) == y.to(device)).sum().item()
+        if outfile_name is not None:
+            with open(outfile_name, 'a') as outfile:
+                outfile.write(f"Adversarial Accuracy on training set: {correct_adv / len(dataloaders['train'].dataset) * 100:.5f} %")
+        print(f"Adversarial Accuracy on training set: {correct_adv / len(dataloaders['train'].dataset) * 100:.5f} %")
+
+        correct = 0
+        with torch.no_grad():
+            for x, y in dataloaders['test']:
+                out = model(x.to(device))
+                correct += (torch.argmax(out, axis=1) == y.to(device)).sum().item()
+        if outfile_name is not None:
+            with open(outfile_name, 'a') as outfile:
+                outfile.write(f"Clean Accuracy on test set: {correct / len(dataloaders['test'].dataset) * 100:.5f} %")
+        print(f"Clean Accuracy on test set: {correct / len(dataloaders['test'].dataset) * 100:.5f} %")
+
+        correct_adv = 0
+        for x, y in dataloaders['test']:
+            out_adv = model(adversary.generate(x.to(device), y.to(device), epsilon=eps))
+            correct_adv += (torch.argmax(out_adv, axis=1) == y.to(device)).sum().item()
+        if outfile_name is not None:
+            with open(outfile_name, 'a') as outfile:
+                outfile.write(f"Adversarial Accuracy on test set: {correct_adv / len(dataloaders['test'].dataset) * 100:.5f} %")
+        print(f"Adversarial Accuracy on test set: {correct_adv / len(dataloaders['test'].dataset) * 100:.5f} %")
+
+
+
+def PGDtrain(model, dataloaders, n_epochs, optimizer, eps, scheduler=None, outfile_name=None, clip=False):
+
+    loss=torch.nn.CrossEntropyLoss()
+    device=torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    if outfile_name is not None:
+        with open(outfile_name, 'w') as outfile:
+            outfile.write("")
+    adversary = PGD(model, 'cuda')
+
+    for epoch in range(n_epochs):
+
+        if outfile_name is not None:
+            with open(outfile_name, 'a') as outfile:
+                outfile.write("\nEpoch: "+str(epoch)+'/'+str(n_epochs))
+        print("Epoch: ", epoch, '/', n_epochs)
+
+        model.train()
+        for x, y in dataloaders['train']:
+            x=x.to(device)
+            y=y.to(device)
+            x_adv = adversary.generate(x, y, epsilon=eps, step_size=eps/3, num_steps=10)
+            out=model(x_adv)
+            l=loss(out, y)
+            optimizer.zero_grad()
+            l.backward()
+            if clip:
+                torch.nn.utils.clip_grad_norm_(model.parameters(), 1.0)
+            optimizer.step()
+        if scheduler is not None:
+            scheduler.step()
+
+        model.eval()
+        correct = 0
+        with torch.no_grad():
+            for x, y in dataloaders['train']:
+                out = model(x.to(device))
+                correct += (torch.argmax(out, axis=1) == y.to(device)).sum().item()
+        if outfile_name is not None:
+            with open(outfile_name, 'a') as outfile:
+                outfile.write(f"\n\nClean Accuracy on training set: {correct / len(dataloaders['train'].dataset) * 100:.5f} %")
+        print(f"\n\nClean Accuracy on training set: {correct / len(dataloaders['train'].dataset) * 100:.5f} %")
+
+        correct_adv = 0
+        for x, y in dataloaders['train']:
+            out_adv = model(adversary.generate(x, y, epsilon=eps, step_size=eps/3, num_steps=10))
+            correct_adv += (torch.argmax(out_adv, axis=1) == y.to(device)).sum().item()
+        if outfile_name is not None:
+            with open(outfile_name, 'a') as outfile:
+                outfile.write(f"Adversarial Accuracy on training set: {correct_adv / len(dataloaders['train'].dataset) * 100:.5f} %")
+        print(f"Adversarial Accuracy on training set: {correct_adv / len(dataloaders['train'].dataset) * 100:.5f} %")
+
+        correct = 0
+        with torch.no_grad():
+            for x, y in dataloaders['test']:
+                out = model(x.to(device))
+                correct += (torch.argmax(out, axis=1) == y.to(device)).sum().item()
+        if outfile_name is not None:
+            with open(outfile_name, 'a') as outfile:
+                outfile.write(f"\nClean Accuracy on test set: {correct / len(dataloaders['test'].dataset) * 100:.5f} %")
+        print(f"\nClean Accuracy on test set: {correct / len(dataloaders['test'].dataset) * 100:.5f} %")
+
+        correct_adv = 0
+        for x, y in dataloaders['test']:
+            out_adv = model(adversary.generate(x, y, epsilon=eps, step_size=eps/3, num_steps=10))
+            correct_adv += (torch.argmax(out_adv, axis=1) == y.to(device)).sum().item()
+        if outfile_name is not None:
+            with open(outfile_name, 'a') as outfile:
+                outfile.write(f"Adversarial Accuracy on test set: {correct_adv / len(dataloaders['test'].dataset) * 100:.5f} %")
+        print(f"Adversarial Accuracy on test set: {correct_adv / len(dataloaders['test'].dataset) * 100:.5f} %")
--- whitebox_training.py
+++ whitebox_training.py
@@ -1,7 +1,8 @@
 import argparse
-#from deeprobust.image.defense.fgsmtraining import FGSMtraining
-from utils.fgsm_train import FGSMtrain
-from deeprobust.image.defense.pgdtraining import PGDtraining
+# from deeprobust.image.defense.fgsmtraining import FGSMtraining
+from utils.adversarial_train import FGSMtrain
+from utils.adversarial_train import PGDtrain
+# from deeprobust.image.defense.pgdtraining import PGDtraining
 from torchvision import transforms
 from utils.data import get_dataloaders
 import torch
@@ -34,25 +35,27 @@ dataloaders = get_dataloaders(data_dir=args.data_dir,
                               test_batch_size=args.test_batch_size,
                               data_transforms=data_transforms)
 
-model_names = ['vgg16']
+model_names = ['tv_resnet50', 'vgg16', 'vit_base_patch16_224']
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-models=[timm.create_model(model_name, pretrained=True, num_classes=10).to(device) for model_name in model_names]
-epsilons=[0.001, 0.005, 0.01, 0.05, 0.1]
+models = [timm.create_model(model_name, pretrained=True, num_classes=10).to(device) for model_name in model_names]
+epsilons = [0.001, 0.005, 0.01, 0.05, 0.1]
 
 for i, model_name in enumerate(model_names):
     for eps in epsilons:
 
         model = models[i].to(device)
         optimizer = torch.optim.SGD(model.parameters(), lr=3e-2, momentum=0.9)
+        '''
         if 'vit' in model_name:
             model.head.load_state_dict(torch.load(f"trained_models/{model_name[4:-4]}.pt"))
         elif 'vgg' in model_name:
             model.head.fc.load_state_dict(torch.load("trained_models/vgg16.pt"))
         else:
             model.fc.load_state_dict(torch.load(f"trained_models/{model_name}.pt"))
+        '''
         model.eval()
-
-        name_model = 'vit_'+model_name[4:-4] if 'vit' in model_name else model_name
+        
+        name_model = 'vit_' + model_name[4:-4] if 'vit' in model_name else model_name
 
         if args.attack == 'FGSM':
             print(f"FGSM Training for {name_model}, eps={eps:.3f}")
@@ -64,20 +67,15 @@ for i, model_name in enumerate(model_names):
                       clip=True)
         elif args.attack == 'PGD':
             print(f"PGD Training for {name_model}, eps={eps:.3f}")
-
-            defense = PGDtraining(model, device)
-            defense.generate(train_loader=dataloaders['train'],
-                             test_loader=dataloaders['test'],
-                             save_dir=f"adversarial_training_results",
-                             save_model=True,
-                             epsilon=eps,
-                             num_steps=10,
-                             perturb_step_size=eps / 3,
-                             save_name=f"{name_model}_adv_training_pgd_eps{eps:.3f}.pt",
-                             epoch_num=15)
+            PGDtrain(model, dataloaders,
+                     n_epochs=10,
+                     optimizer=optimizer,
+                     outfile_name=f"./training_outputs/{name_model}_{args.attack}_eps{eps:.3f}.txt",
+                     eps=eps,
+                     clip=True)
 
         if model_name == 'vgg16':
-            torch.save(model.head.fc.state_dict(), f"trained_models/{model_name}_{args.attack}_eps{eps:.3f}.pt")  # to save memory
+            torch.save(model.head.fc.state_dict(),
+                       f"trained_models/{model_name}_{args.attack}_eps{eps:.3f}.pt")  # to save memory
         else:
-            torch.save(model.state_dict(),  f"trained_models/{model_name}_{args.attack}_eps{eps:.3f}.pt")
-
+            torch.save(model.state_dict(), f"trained_models/{model_name}_{args.attack}_eps{eps:.3f}.pt")
