dd463897df9bff012e2299dba58467461e83a65b
Roberto <robi290596@gmail.com>
2021-06-28 21:23:36 +0200

    re-created FGSM training + modify whitebox_training.py

--- whitebox_training.py
+++ whitebox_training.py
@@ -1,5 +1,6 @@
 import argparse
-from deeprobust.image.defense.fgsmtraining import FGSMtraining
+#from deeprobust.image.defense.fgsmtraining import FGSMtraining
+from utils.fgsm_train import FGSMtrain
 from deeprobust.image.defense.pgdtraining import PGDtraining
 from torchvision import transforms
 from utils.data import get_dataloaders
@@ -33,7 +34,7 @@ dataloaders = get_dataloaders(data_dir=args.data_dir,
                               test_batch_size=args.test_batch_size,
                               data_transforms=data_transforms)
 
-model_names = ['tv_resnet50', 'vgg16']
+model_names = ['vgg16']
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 models=[timm.create_model(model_name, pretrained=True, num_classes=10).to(device) for model_name in model_names]
 epsilons=[0.001, 0.005, 0.01, 0.05, 0.1]
@@ -42,7 +43,7 @@ for i, model_name in enumerate(model_names):
     for eps in epsilons:
 
         model = models[i].to(device)
-
+        optimizer = torch.optim.SGD(model.parameters(), lr=3e-2, momentum=0.9)
         if 'vit' in model_name:
             model.head.load_state_dict(torch.load(f"trained_models/{model_name[4:-4]}.pt"))
         elif 'vgg' in model_name:
@@ -55,14 +56,12 @@ for i, model_name in enumerate(model_names):
 
         if args.attack == 'FGSM':
             print(f"FGSM Training for {name_model}, eps={eps:.3f}")
-            defense = FGSMtraining(model, device)
-            defense.generate(train_loader=dataloaders['train'],
-                             test_loader=dataloaders['test'],
-                             save_dir=f"adversarial_training_results",
-                             save_model=True,
-                             epsilon = eps,
-                             save_name=f"{name_model}_adv_training_fgsm_eps{eps:.3f}.pt",
-                             epoch_num=args.num_epochs)
+            FGSMtrain(model, dataloaders,
+                      n_epochs=10,
+                      optimizer=optimizer,
+                      outfile_name=f"./training_outputs/{name_model}_{args.attack}_eps{eps:.3f}.txt",
+                      eps=eps,
+                      clip=True)
         elif args.attack == 'PGD':
             print(f"PGD Training for {name_model}, eps={eps:.3f}")
 
@@ -76,4 +75,9 @@ for i, model_name in enumerate(model_names):
                              perturb_step_size=eps / 3,
                              save_name=f"{name_model}_adv_training_pgd_eps{eps:.3f}.pt",
                              epoch_num=15)
+
+        if model_name == 'vgg16':
+            torch.save(model.head.fc.state_dict(), f"trained_models/{model_name}_{args.attack}_eps{eps:.3f}.pt")  # to save memory
+        else:
+            torch.save(model.state_dict(),  f"trained_models/{model_name}_{args.attack}_eps{eps:.3f}.pt")
 
