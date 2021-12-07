fixed neighbourhood computation

neighbourhood.py
@@ -19,7 +19,7 @@ dataloaders = get_dataloaders(data_dir=args.data,
                               train_batch_size=args.train_batch_size,
                               test_batch_size=args.test_batch_size)
 # get device
-device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+device = torch.device("cuda:1" if torch.cuda.is_available() else "cpu")
 print("Device:", device)
 
 # generate pretrained models with timm

utils/neighbours.py
@@ -1,4 +1,5 @@
 import torch
+import numpy as np
 
 def multidim_select(tensor, indices):
     output_tensor=torch.zeros((tensor.shape[0], indices.shape[1]))
@@ -7,20 +8,26 @@ def multidim_select(tensor, indices):
     return output_tensor
 
 def extract_neighbourhoods(model, dataloader, k=10):
-    N=len(dataloader.dataset)
-    for i, (x,y) in enumerate(dataloader):
-        print("Batch: ", i)
-        _, repr=model(x)
-        if i==0:
-            k_dist=torch.ones((len(repr), N, k))*torch.inf
-            k_neighbours=torch.zeros((len(repr), N, k), dtype=torch.int)
-        for j, (xp, yp) in enumerate(dataloader):
-            _, reprp=model(xp)
-            for h in range(n_representations):
-                distances, neighbours=[a[:,:k] for a in torch.cdist(representations[h], representationsp[h]).sort(dim=1)]
-                neighbours+=j*x.shape[0]
-                extended_dist=torch.cat([k_dist[h,i*x.shape[0]:(i+1)*x.shape[0]], distances], axis=1)
-                indices=extended_dist.argsort(dim=1)[:,:k]
-                k_dist[h, i*x.shape[0]:(i+1)*x.shape[0]]=multidim_select(extended_dist,indices)
-                k_neighbours[h, i*x.shape[0]:(i+1)*x.shape[0]]=multidim_select(torch.cat([k_neighbours[h,i*x.shape[0]:(i+1)*x.shape[0]], neighbours], axis=1), indices)
+    with torch.no_grad():
+        device=torch.device('cuda:1')
+        N=len(dataloader.dataset)
+        for i, (x,y) in enumerate(dataloader):
+            x=x.to(device)
+            print("Batch: ", i)
+            _, repr=model(x)
+            if i==0:
+                k_dist=torch.ones((len(repr), N, k)).to(device)*np.inf
+                k_neighbours=torch.zeros((len(repr), N, k), dtype=torch.int).to(device)
+            for j, (xp, yp) in enumerate(dataloader):
+                torch.cuda.empty_cache()
+                xp=xp.to(device)
+                _, reprp=model(xp)
+                for h in range(len(repr)):
+                    distances, neighbours=[a[:,:k] for a in torch.cdist(repr[h].reshape(repr[h].shape[0], -1), reprp[h].reshape(reprp[h].shape[0], -1)).sort(dim=1)]
+                    neighbours=neighbours.int()
+                    neighbours+=j*x.shape[0]
+                    extended_dist=torch.cat([k_dist[h,i*x.shape[0]:(i+1)*x.shape[0]], distances], axis=1)
+                    indices=extended_dist.argsort(dim=1)[:,:k]
+                    k_dist[h, i*x.shape[0]:(i+1)*x.shape[0]]=multidim_select(extended_dist,indices)
+                    k_neighbours[h, i*x.shape[0]:(i+1)*x.shape[0]]=multidim_select(torch.cat([k_neighbours[h,i*x.shape[0]:(i+1)*x.shape[0]], neighbours], axis=1), indices)
     return k_neighbours